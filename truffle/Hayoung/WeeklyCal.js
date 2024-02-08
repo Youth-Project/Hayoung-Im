52d9366fc75045ed19ccfc5cc58e551e79eb48df
Hayoung-Im <143571041+Hayoung-Im@users.noreply.github.com>
2024-02-09 01:59:07 +0900

    Update WeeklyCal.js

--- truffle/Hayoung/WeeklyCal.js
+++ truffle/Hayoung/WeeklyCal.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState, userEffect } from 'react';
 import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
 import moment from 'moment';
 import Svg, { Circle, Text as SvgText } from 'react-native-svg';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const WeeklyCal = ({ selectedDate, handleDayClick }) => {
-  
+  const [amounts, setAmounts] = useState({});
   const startOfWeek = moment(selectedDate).startOf('week');
   const weekDays = Array.from({ length: 7 }, (_, index) => moment(startOfWeek).add(index, 'day'));
   const circleRadius = 16;
+
+  useEffect(() => {
+    getAmountsForWeek(weekDays).then((amounts) => {
+      setAmounts(amounts);
+    });
+  }, [selectedDate]);
+
+  const getAmountForDate = async (date) => {
+    try {
+      const userId = firebase.auth().currentUser.uid;
+      const docRef = firebase.firestore().collection(userId).doc(date);
+      const docSnapshot = await docRef.get();
+
+      if (docSnapshot.exists) {
+        const data = docSnapshot.data();
+        return data.amount || 0;
+      } else {
+        return 0;
+      }
+    } catch (error) {
+      console.error('Error fetching amount from date:', error);
+      return 0;
+    }
+  };
+
+  const getAmountsForWeek = async (weekDays) => {
+    const amounts = {};
+    for (let day of weekDays) {
+      const amount = await getAmountForDate(day.format('YYYY-MM-DD'));
+      amounts[day.format('YYYY-MM-DD')] = amount;
+    }
+    return amounts;
+  };
+  
   return (
     <View style={styles.container}>
       {weekDays.map((day, index) => (
@@ -39,6 +75,8 @@ const WeeklyCal = ({ selectedDate, handleDayClick }) => {
               >
                 {day.format('DD')}
               </SvgText>
+                // 지출금액 나오는 텍스트
+                {amounts[day.format('YYYY-MM-DD')]}   
             </Svg>
           </View>
         </TouchableOpacity>
